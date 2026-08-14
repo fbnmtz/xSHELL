39cb8cf056f6c3be2d2e177f0c0bc0c9e2e6ebea
Fabiano Matos <fabiano.matoz@gmail.com>
2026-08-14 13:52:46 -0300

    add xshell env script with config vars

--- examples/xshell_env.sh
+++ examples/xshell_env.sh
@@ -2,14 +2,14 @@
 #
 # ############################################################################
 # Project: xSHELL (none)
-# File...: examples/env.template
+# File...: examples/xshell_env.sh
 # Created: Saturday, 2026/08/08 - 11:33:09
 # Author.: @fbnmtz, (fabiano.matoz@gmail.com)
 # ~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~~·~·~·~·~·~·~·~
-# Last Modified: Sunday, 2026/08/09 - 19:25:08
+# Last Modified: Friday, 2026/08/14 - 13:51:53
 # Modified By..: @fbnmtz, (fabiano.matoz@gmail.com)
 # ~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~~·~·~·~·~·~·~·~
-# Version: 0.0.2.13
+# Version: 0.0.2.20
 # ~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~·~·~·~·~·~·~·~~·~·~·~·~·~~·~·~·~·~·~·~·~
 # Description: 
 #  >
@@ -17,14 +17,14 @@
 # HISTORY:
 #
 
-# environment variables for xSHELL. Need to change installation (xshell path)
-# this should be add in 
-#  ~/.bashrc or ~/.zshrc or ~/.profile or ~/.bash_profile or ~/.bash_login or ~/.login
-#  /etc/profile or /etc/bash.bashrc or /etc/zsh/zshrc or /etc/zsh/zprofile
-export xSHELL_PATH="/opt/xSHELL"
+# environment variables for xSHELL
+# this should be add in  ~/.profile and /etc/profile
+
+export xSHELL_PATH="/opt/xSHELL" #change me for your current install dir
 export xSHELL_INIT="${xSHELL_PATH}/init"
 export xSHELL_BIN="${xSHELL_PATH}/bin"
 export xSHELL_SCRIPTS="${xSHELL_PATH}/scripts"
 export xSHELL_EXAMPLES="${xSHELL_PATH}/examples"
 export PATH="${PATH}:${xSHELL_PATH}:${xSHELL_BIN}:${xSHELL_SCRIPTS}:${xSHELL_EXAMPLES}:${HOME}/bin"
+# shellcheck disable=SC1091
 xs_init(){ source "${xSHELL_PATH}/init" ; }
